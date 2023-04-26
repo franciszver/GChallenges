35c7fcf653a44c6204111e35eb3537cabafe4660
franciszver <franciszver@outlook.com>
2023-04-25 23:15:28 -0700

    more success

--- Challenges/fuelInjection.py
+++ Challenges/fuelInjection.py
@@ -1,44 +1,23 @@
-testInt = '1023'
-expected = 1
-
 def solution(n):
     n = int(n)
-    previous = 1
-    total = 1
     steps = 0
-    forwards = 0
-    backwards = 0
-    if n <= 2:
-        return steps
-    for i in range(n):
-        if total == n:
-            return steps
-        if total > n:
-            #check subtract
-            backwards = 0
-            for j in range(total*2):
-                if total-j > n:
-                    backwards = backwards + 1
-                else:
-                    break
-            #check add
-            forwards = 0
-            for k in range(total*2):
-                if previous+k < n:
-                    forwards = forwards + 1
-                else:
-                    break
-            if forwards == backwards:
-                steps = steps + forwards-1
-            if forwards < backwards:
-                steps = steps + forwards-1
-            if forwards > backwards:
-                steps = steps + backwards
-            return steps
-        previous = total
-        total = total * 2
-        steps = steps + 1
+    if n == 1:
+        return 0
+    while n > 1:
+        print(n)
+        if n%2 == 0:
+            steps = steps+1
+            n = n/2
+        else:
+            #test up and down
+            if ((n-1)/2)%2 == 0:
+                steps = steps + 1
+                n = n-1
+            else:
+                steps = steps + 1
+                n = n+1
     return steps
+    
 
 for i in range(100):
     print(solution(i), i)
