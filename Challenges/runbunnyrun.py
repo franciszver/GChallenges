26328b2109dcf12e9aa02a677bcea661796304ba
franciszver <franciszver@outlook.com>
2023-05-02 18:14:51 -0700

    initial comments

---
+++ Challenges/runbunnyrun.py
@@ -0,0 +1,11 @@
+# this is a pathway challenge
+# gotta get to the door at the end under a certain amount of time
+# the hint is the bellman-ford algorithm: https://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm
+# a good explanation of the challenge can be found by this dev: https://github.com/aswintechguy?tab=repositories
+
+
+
+# solution.solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3)Output:    [0, 1]
+
+# solution.solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1)Output:    [1, 2]
+
