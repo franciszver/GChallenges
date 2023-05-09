fd483e53e6eeb1038fce639c4d9057fbd3b040d3
franciszver <franciszver@outlook.com>
2023-05-08 21:09:02 -0700

    This is the start of my theory on quick thinking

---
+++ Challenges/Trainer_Angles.py
@@ -0,0 +1,15 @@
+
+
+
+# Tests to try:
+#     assert solution([3, 2], [1, 1], [2, 1], 4) == 7
+#     assert solution([2, 5], [1, 2], [1, 4], 11) == 27
+#     assert solution([23, 10], [6, 4], [3, 2], 23) == 8
+#     assert solution([1250, 1250], [1000, 1000], [500, 400], 10000) == 196
+#     assert solution([10, 10], [4, 4], [3, 3], 5000) == 739323
+#     assert solution([3, 2], [1, 1], [2, 1], 7) == 19
+#     assert solution([2, 3], [1, 1], [1, 2], 4) == 7
+#     assert solution([3, 4], [1, 2], [2, 1], 7) == 10
+#     assert solution([4, 4], [2, 2], [3, 1], 6) == 7
+#     assert solution([300, 275], [150, 150], [180, 100], 500) == 9
+#     assert solution([3, 4], [1, 1], [2, 2], 500) == 54243
